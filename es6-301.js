5a0b36b1431616185b22ebaf1115c06bfc799cf6
thankyou123456789 <69336071+thankyou123456789@users.noreply.github.com>
2020-12-01 11:22:10 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,17 @@
+// the string
+// 1.the basic Syntax of String
+// let message = "Hello World " ;
+// console.log(message);
+// 2.The basic Syntax of String
+let fname = "Mark";
+let lname = " Sakaberg";
+let fullName = fname + " " + lname;
+// console.log(fname + "" + lname);
+console.log(fullName);
+//The string with numver
+console.log("Hello" + 12); // Hello12
+console.log("10" + 2); //102
+
+console.log("10" == 10);//true
+console.log("10" === 10);//false
+console.log("10" !== 10); //true
